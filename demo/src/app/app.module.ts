802df8cdfa7a5e97df9fe4793e2ddfe3dcaf170d
584292580@qq.com <xc15801654323>
2017-08-05 00:08:27 +0800

    demo 1

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -3,12 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ProductionComponent } from './production/production.component';
+import { AdminComponent } from './admin/admin.component';
+
+import { RouterModule } from '@angular/router';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductionComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path:'admin',
+        component:AdminComponent
+      },
+      {
+        path:'production',
+        component:ProductionComponent
+      }
+    ])
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
